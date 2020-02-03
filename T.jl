5efc47fbaed721f09a85ae8bad37d74ad3ad4fae
s1654645 <60391429+s1654645@users.noreply.github.com>
2020-02-03 12:28:14 +0000

    Add files via upload

---
+++ T.jl
@@ -0,0 +1,38 @@
+# read the data from file
+using ExcelReaders
+t_e55_file = readxl("C:\\Users\\Carol Clark\\OneDrive\\Dissertation\\Data\\test.xlsx", "T!A2:A5") #read data from file
+t_e6_file = readxl("C:\\Users\\Carol Clark\\OneDrive\\Dissertation\\Data\\test.xlsx", "T!B2:B9") #read data from file
+t_e65_file = readxl("C:\\Users\\Carol Clark\\OneDrive\\Dissertation\\Data\\test.xlsx", "T!C2:C15") #read data from file
+t_e7_file = readxl("C:\\Users\\Carol Clark\\OneDrive\\Dissertation\\Data\\test.xlsx", "T!D2:D43") #read data from file
+# t_e75_file = readxl("C:\\Users\\Carol Clark\\OneDrive\\Dissertation\\Data\\test.xlsx", "T!A1:A42") #read data from file
+
+# convert to array of Float64
+t_e55 = convert.(Float64, t_e55_file) #convert from array of any to array of Float64 and store as new variable
+t_e6 = convert.(Float64, t_e6_file) #convert from array of any to array of Float64 and store as new variable  
+t_e65 = convert.(Float64, t_e65_file) #convert from array of any to array of Float64 and store as new variable 
+t_e7 = convert.(Float64, t_e7_file) #convert from array of any to array of Float64 and store as new variable 
+# t_e75 = convert.(Float64, t_e75_file) #convert from array of any to array of Float64 and store as new variable 
+
+# calculate entropy
+using InformationMeasures
+ent_t_e55 = get_entropy(t_e55) #calculate entropy 
+ent_t_e6 = get_entropy(t_e6) #calculate entropy 
+ent_t_e65 = get_entropy(t_e65) #calculate entropy 
+ent_t_e7 = get_entropy(t_e7) #calculate entropy 
+# ent_t_e75 = get_entropy(t_e75) #calculate entropy 
+
+
+# print entropy 
+println("entropy T e5.5 is ", ent_t_e55) #print entropy
+println("entropy T e6 is ", ent_t_e6) #print entropy
+println("entropy T e6.5 is ", ent_t_e65) #print entropy
+println("entropy T e7 is ", ent_t_e7) #print entropy
+# println("entropy T e7.5 is ", ent_t_e75) #print entropy
+
+# display histogram 
+using Plots
+display(histogram(t_e55)) #plot hisogram of data 
+display(histogram(t_e6)) #plot hisogram of data 
+display(histogram(t_e65)) #plot hisogram of data 
+display(histogram(t_e7)) #plot hisogram of data 
+# display(histogram(t_e75)) #plot hisogram of data 
