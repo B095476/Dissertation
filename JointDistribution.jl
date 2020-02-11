ba4ddcbae12e1906e2582e55106104e52a2be3ad
s1654645 <60391429+s1654645@users.noreply.github.com>
2020-02-11 14:25:51 +0000

    Add files via upload

--- JointDistribution.jl
+++ JointDistribution.jl
@@ -1,16 +1,16 @@
 using Plots
 
 #e6
-display(scatter([n_e6, t_e6], color=[:purple :yellow], label=["Nanog" "T"], title="Joint Distribution Nanog and T at E6"))
-display(scatter([n_e6, s_e6], color=[:purple :lightblue], label=["Nanog" "Sox2"], title="Joint Distribution Nanog and Sox2 at E6"))
-display(scatter([s_e6, t_e6], color=[:lightblue :yellow], label=["Nanog" "T"], title="Joint Distribution Sox2 and T at E6"))
+display(marginalhist(n_e6, t_e6, xaxis="Nanog E6", yaxis="T E6"))
+display(marginalhist(n_e6, s_e6, xaxis="Nanog E6", yaxis="Sox2 E6"))
+display(marginalhist(s_e6, t_e6, xaxis="Sox2 E6", yaxis="T E6"))
 
 #e65
-display(scatter([n_e65, t_e65], color=[:purple :yellow], label=["Nanog" "T"], title="Joint Distribution Nanog and T at E6.5"))
-display(scatter([n_e65, s_e65], color=[:purple :lightblue], label=["Nanog" "Sox2"], title="Joint Distribution Nanog and Sox2 at E6.5"))
-display(scatter([s_e65, t_e65], color=[:lightblue :yellow], label=["Nanog" "T"], title="Joint Distribution Sox2 and T at E6.5"))
+display(marginalhist(n_e65, t_e65, xaxis="Nanog E6.5", yaxis="T E6.5"))
+display(marginalhist(n_e65, s_e65, xaxis="Nanog E6.5", yaxis="Sox2 E6.5"))
+display(marginalhist(s_e65, t_e65, xaxis="Sox2 E6.5", yaxis="T E6.5"))
 
 #e7
-display(scatter([n_e7, t_e7], color=[:purple :yellow], label=["Nanog" "T"], title="Joint Distribution Nanog and T at E7"))
-display(scatter([n_e7, s_e7], color=[:purple :lightblue], label=["Nanog" "Sox2"], title="Joint Distribution Nanog and Sox2 at E7"))
-display(scatter([s_e7, t_e7], color=[:lightblue :yellow], label=["Nanog" "T"], title="Joint Distribution Sox2 and T at E7"))
+display(marginalhist(n_e7, t_e7, xaxis="Nanog E7", yaxis="T E7"))
+display(marginalhist(n_e7, s_e7, xaxis="Nanog E7", yaxis="Sox2 E7"))
+display(marginalhist(s_e7, t_e7, xaxis="Sox2 E7", yaxis="T E7"))
